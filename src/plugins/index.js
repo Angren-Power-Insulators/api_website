9e1e42dd5c1885f8d544661c562718565a52443e
Bekzod <notarious2@gmail.com>
2025-02-22 13:46:27 +0500

    add Google Analytics

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,10 +8,14 @@
 import vuetify from './vuetify'
 import pinia from '@/stores'
 import router from '@/router'
+import VueGtag from 'vue-gtag'
 
-export function registerPlugins (app) {
+export function registerPlugins(app) {
   app
     .use(vuetify)
     .use(router)
     .use(pinia)
+    .use(VueGtag, {
+      config: { id: import.meta.env.VITE_GA_MEASUREMENT_ID },
+    })
 }
